e23e1a81460cfe8b95e75796cabc1838c7b17eec
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-06-05 16:58:51 +0100

    Loop throw

--- Script/index.js
+++ Script/index.js
@@ -1,7 +1,23 @@
 function loadData(){
+    // Fetch Data from API
     fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+    // Convert Response to json
     .then((res)=>res.json())
-    .then((data)=>console.log(data.categories));
+    // Send data to display category
+    .then((data)=>displayCategory(data.categories));
 }
 
+ // {
+//     "category_id": "1001",
+//     "category": "Music"
+// }
+function displayCategory(categories){
+    // Get the Container
+    const categoryContainer = document.getElementById('category-container');
+    // As We got array of Object then we Operate Loop
+    for(let cat of categories){
+        console.log(cat);
+    }
+    // Every Single element we got need to append 
+}
 loadData();
