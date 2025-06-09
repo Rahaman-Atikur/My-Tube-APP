36cba618071748f63e032cf63dbf8b29f473cc36
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-06-09 20:45:13 +0100

    Search Methodlogy Done

--- Script/index.js
+++ Script/index.js
@@ -13,14 +13,42 @@ function loadData() {
     // Send data to display category
     .then((data) => displayCategory(data.categories));
 }
-function loadVideos() {
-  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+function loadVideos(searchText ="") {
+  fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`)
     .then((response) => response.json())
     .then((data) => {
        removeActiveClass();
       document.getElementById('btn-all').classList.add('active');
       displayVideos(data.videos);
     } );
+}
+const loadVideoDetails =(videoId)=>{
+  // console.log(videoId);
+  const url = `https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`;
+  fetch(url)
+  .then((res)=>res.json())
+  .then((data)=>displayVideoDetails(data.video));
+}
+const displayVideoDetails=(video)=>{
+  console.log(video);
+  document.getElementById("video_details").showModal();
+ const detailsContainer= document.getElementById('details-container');
+ detailsContainer.innerHTML=`
+ <div class="card bg-base-100 image-full w-96 shadow-sm">
+  <figure>
+    <img
+      src="${video.thumbnail}"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.title}</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Play</button>
+    </div>
+  </div>
+</div>
+ `;
 }
 function loadCategoryVideos(id){
   // console.log(id);
@@ -82,17 +110,26 @@ return;
     <div class="intro">
         <h2 class="text-sm font-semibold">${video.title}</h2>
         <p class="text-sm text-gray-400 flex gap-1">${video.authors[0].profile_name}
-            <img class="w-5 h-5" src="https://img.icons8.com/?size=96&id=FNbnqlDTjR45&format=gif" alt="">
+        ${video.authors[0].verified == true ? `<img class="w-5 h-5" src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png" >`:``}
+            
         </p>
         <p class="text-sm text-gray-400 ">${video.others.views}</p>
     </div>
     
     </div>
+     <button onclick="loadVideoDetails('${video.video_id}')" class="btn btn-block">Show Details</button>
   </div>
     `;
     videoContainer.append(videoCard);
   });
 };
+document.getElementById("search-input").addEventListener("keyup",(e)=>{
+  const input=e.target.value;
+  // console.log(input);
+  loadVideos(input);
+})
+
+
 loadData();
 loadVideos();
 
